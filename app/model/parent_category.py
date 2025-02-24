407b84ad363f479c5c5b181d008d891b59bea8e4
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-24 10:09:19 +0100

    y

--- app/model/parent_category.py
+++ app/model/parent_category.py
@@ -1,12 +1,4 @@
 class Parent_category:
-	children = []
-
 	def __init__(self, id, name):
 		self.id = id
 		self.name = name
-
-	def add_child(self, child_category):
-		self.children.append(child_category)
-
-	def __str__(self):
-		return(f"{self.id} {self.name}")
