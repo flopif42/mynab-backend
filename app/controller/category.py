e26f19d683d6e741cf1a2e41e8e8cbe859c2551d
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-24 11:52:56 +0100

    x

--- app/controller/category.py
+++ app/controller/category.py
@@ -17,8 +17,10 @@ def fetch_all(id_user, unused):
         parent_categories = db.execute_query(query, (str(id_user),), fetch=True, dictionary=True)
         for parent_category in parent_categories:
             query_children = (
-                "select ID_CATEGORY as id, CATEGORY_NAME as name, ID_PARENT_CATEGORY as id_parent, 0 as can_be_deleted "
-                "from CATEGORY where ID_USER = (%s) and ID_PARENT_CATEGORY = (%s)"
+                "select cat.ID_CATEGORY as id, CATEGORY_NAME as name, ID_PARENT_CATEGORY as id_parent, count(ID_TRANSACTION) as can_be_deleted "
+                "from CATEGORY cat left join TRANSACTION txn on txn.ID_CATEGORY = cat.ID_CATEGORY "
+                "where ID_USER = (%s) and ID_PARENT_CATEGORY = (%s) "
+                "group by cat.ID_CATEGORY , CATEGORY_NAME , ID_PARENT_CATEGORY "
             )
             categories = db.execute_query(query_children, (id_user, parent_category['id']), fetch=True, dictionary=True)
             parent_category['child_categories'] = []
