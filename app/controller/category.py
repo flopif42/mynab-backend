80a2d09c62c651e556127a69f4e3540e16e1298c
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-24 10:00:48 +0100

    x

--- app/controller/category.py
+++ app/controller/category.py
@@ -18,7 +18,7 @@ def fetch_all(id_user, unused):
         result = db.execute_query(query, (str(id_user),), fetch=True, dictionary=True)
 
         for (id, name) in result:
-            parent_categories.append(new Parent_category(id, name))
+            parent_categories.append(Parent_category(id, name))
 
         print(parent_categories)
 
