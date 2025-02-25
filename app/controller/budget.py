99d3174fea6a6ea554cf782a4ce1a6c351cbc421
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-25 17:57:03 +0100

    z

--- app/controller/budget.py
+++ app/controller/budget.py
@@ -13,7 +13,7 @@ def fetch(id_user, unused):
             "on e.EXP_YEAR = p.YEAR and e.EXP_MONTH = p.MONTH and e.ID_CATEGORY = p.ID_CATEGORY and e.ID_USER = p.ID_USER "
             "where p.ID_USER = (%s) "
         )
-        result = db.execute_query(query, (id_user,), fetch=True, dictionary=True)
+        result = db.execute_query(query, (id_user,), fetch=True)
         for row in result:
             (year, month, id_category, funded, spent) = row
             print(row)
