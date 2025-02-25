bb2dd2eb2a41fa84e3906acb51308da967c445c0
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-25 17:41:12 +0100

    x

--- app/controller/budget.py
+++ app/controller/budget.py
@@ -3,7 +3,7 @@ import app.db as db
 def fetch(id_user, unused):
     try:
         query = (
-            "select p.ID_USER, p.YEAR, p.MONTH, p.ID_CATEGORY, cat.CATEGORY_NAME, BUDGET_LINE_AMOUNT, ifnull(EXP_AMOUNT, 0) "
+            "select p.YEAR as year, p.MONTH as month, p.ID_CATEGORY as id_category, ifnull(BUDGET_LINE_AMOUNT, 0) as funded, ifnull(EXP_AMOUNT, 0) as spent "
             "from BUDGET_PERIOD p "
             "left join BUDGET_LINE bl "
 	        "on bl.BUDGET_LINE_YEAR = p.YEAR and bl.BUDGET_LINE_MONTH = p.MONTH "
@@ -11,8 +11,6 @@ def fetch(id_user, unused):
             "left join EXPENSES e "
             "on e.EXP_YEAR = p.YEAR and e.EXP_MONTH = p.MONTH "
             "and e.ID_CATEGORY = p.ID_CATEGORY and e.ID_USER = p.ID_USER "
-            "left join CATEGORY cat "
-            "on cat.ID_USER = p.ID_USER and cat.ID_CATEGORY = p.ID_CATEGORY "
             "where p.ID_USER = (%s) "
         )
         result = db.execute_query(query, (id_user,), fetch=True, dictionary=True)
