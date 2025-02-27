f471d1c3e0671c87fe5d87fdccf7f05743113264
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-27 09:36:06 +0100

    y

--- app/controller/budget.py
+++ app/controller/budget.py
@@ -4,19 +4,19 @@ def fetch(id_user, unused):
     budget_tmp = {}
     budget = []
     try:
-        query = (
-            "select p.YEAR as year, p.MONTH as month, p.ID_CATEGORY as id, cat.CATEGORY_NAME as name, "
-            "ifnull(BUDGET_LINE_AMOUNT, 0) as funded, "
-            "ifnull(EXP_AMOUNT, 0) as spent "
-            "from BUDGET_PERIOD p "
-            "left join BUDGET_LINE bl "
-	        "on bl.BUDGET_LINE_YEAR = p.YEAR and bl.BUDGET_LINE_MONTH = p.MONTH and bl.ID_CATEGORY = p.ID_CATEGORY and bl.ID_USER = p.ID_USER "
-            "left join EXPENSES e "
-            "on e.EXP_YEAR = p.YEAR and e.EXP_MONTH = p.MONTH and e.ID_CATEGORY = p.ID_CATEGORY and e.ID_USER = p.ID_USER "
-            "left join CATEGORY cat "
-            "on cat.ID_USER = p.ID_USER and cat.ID_CATEGORY = p.ID_CATEGORY "
-            "where p.ID_USER = (%s) "
-        )
+        query = """"
+            select p.YEAR as year, p.MONTH as month, p.ID_CATEGORY as id, cat.CATEGORY_NAME as name, 
+            ifnull(BUDGET_LINE_AMOUNT, 0) as funded, 
+            ifnull(EXP_AMOUNT, 0) as spent 
+            from BUDGET_PERIOD p 
+            left join BUDGET_LINE bl 
+	        on bl.BUDGET_LINE_YEAR = p.YEAR and bl.BUDGET_LINE_MONTH = p.MONTH and bl.ID_CATEGORY = p.ID_CATEGORY and bl.ID_USER = p.ID_USER 
+            left join EXPENSES e 
+            on e.EXP_YEAR = p.YEAR and e.EXP_MONTH = p.MONTH and e.ID_CATEGORY = p.ID_CATEGORY and e.ID_USER = p.ID_USER 
+            left join CATEGORY cat 
+            on cat.ID_USER = p.ID_USER and cat.ID_CATEGORY = p.ID_CATEGORY 
+            where p.ID_USER = (%s) 
+        """"
         result = db.execute_query(query, (id_user,), fetch=True, dictionary=True)
         for budget_line in result:
             year = budget_line.pop('year')
