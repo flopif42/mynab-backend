d10ac6453af85a5156369c1127cb95791abbf46e
Florent Nguyen <nguyen.florent@gmail.com>
2025-02-28 15:04:52 +0100

    y

--- app/controller/budget.py
+++ app/controller/budget.py
@@ -37,3 +37,19 @@ group by year, month, id
     except Exception as err:
         print(f"Could not fetch budget : {err}")
         raise
+
+def set_funded(id_user, request_params):
+    try:
+        id_period = request_params['id_period']
+        budget_line_year = int(id_period[:4])
+        budget_line_month = int(id_period[5:])
+        amount = int(request_params['funded'])
+        query = """
+insert into BUDGET_LINE (ID_USER, ID_CATEGORY, BUDGET_LINE_YEAR, BUDGET_LINE_MONTH, BUDGET_LINE_AMOUNT) 
+values ((%s), (%s), (%s), (%s), (%s))
+"""
+        values = (id_user, request_params['id_category'], budget_line_year, budget_line_month, amount)
+        db.execute_query(query, values, commit=True)
+    except Exception as err:
+        print(f"Could not create the budget line : {err}")
+        raise
--- app/routes/budget_routes.py
+++ app/routes/budget_routes.py
@@ -5,5 +5,9 @@ from app.controller import budget
 budget_bp = Blueprint('budget', __name__)
 
 @budget_bp.route('/budget/list', methods=['GET'])
-def budge_list():
+def budget_list():
     return handle_route_action(budget.fetch)
+
+@budget_bp.route('/budget/set_funded', methods=['POST'])
+def budget_set_funded():
+    return handle_route_action(budget.set_funded)
