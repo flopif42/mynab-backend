b1c900c218292d822caad7fa96364313f0b20107
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-03 16:46:39 +0100

    y

--- app/controller/budget.py
+++ app/controller/budget.py
@@ -27,7 +27,6 @@ def fetch(id_user, unused):
                 """
         result = db.execute_query(query, (id_user,), fetch=True, dictionary=True)
         for row in result:
-            print(row)
             year = row.pop('year')
             month = row.pop('month')
             id_period = f"{year}_{month:02d}"
