bea978d47d0d86ed1085b293598bcfaffa79e5d6
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-04 11:33:03 +0100

    x

--- app/controller/budget.py
+++ app/controller/budget.py
@@ -9,20 +9,20 @@ def fetch(id_user, unused):
     try:
         query = """
                 select p.YEAR as year, p.MONTH as month, cat.ID_CATEGORY as id,
-                  ifnull(i.INC_AMOUNT, 0) as total_income, ifnull(atb.available, 0) as available,
-                  bl.BUDGET_LINE_AMOUNT as funded, e.EXP_AMOUNT as spent,
-                  sum(ifnull(BUDGET_LINE_AMOUNT, 0) + ifnull(EXP_AMOUNT, 0)) over(partition by cat.ID_CATEGORY order by year, month rows between unbounded preceding and current row) as remaining
+                    ifnull(i.INC_AMOUNT, 0) as total_income, ifnull(atb.available, 0) as available,
+                    bl.BUDGET_LINE_AMOUNT as funded, e.EXP_AMOUNT as spent,
+                    sum(ifnull(BUDGET_LINE_AMOUNT, 0) + ifnull(EXP_AMOUNT, 0)) over(partition by cat.ID_CATEGORY order by year, month rows between unbounded preceding and current row) as remaining
                 from BUDGET_PERIOD p
                 left join CATEGORY cat
 	                on cat.ID_USER = p.ID_USER
                 left join AVAILABLE_TO_BUDGET atb
-                  on atb.ID_USER = p.ID_USER and atb.YEAR = p.YEAR and atb.month = p.MONTH and cat.ID_CATEGORY = 0
+                    on atb.ID_USER = p.ID_USER and atb.YEAR = p.YEAR and atb.month = p.MONTH and cat.ID_CATEGORY = 0
                 left join EXPENSES e
 	                on e.ID_USER = p.ID_USER and e.EXP_YEAR = p.YEAR and e.EXP_MONTH = p.MONTH and e.ID_CATEGORY = cat.ID_CATEGORY
                 left join INCOME i
-                  on i.ID_USER = p.ID_USER and i.INC_YEAR = p.YEAR and i.INC_MONTH = p.MONTH and cat.ID_CATEGORY = 0
+                    on i.ID_USER = p.ID_USER and i.INC_YEAR = p.YEAR and i.INC_MONTH = p.MONTH and cat.ID_CATEGORY = 0
                 left join BUDGET_LINE bl
-                  on bl.ID_USER = p.ID_USER and bl.BUDGET_LINE_YEAR = p.YEAR and bl.BUDGET_LINE_MONTH = p.MONTH and bl.ID_CATEGORY = cat.ID_CATEGORY
+                    on bl.ID_USER = p.ID_USER and bl.BUDGET_LINE_YEAR = p.YEAR and bl.BUDGET_LINE_MONTH = p.MONTH and bl.ID_CATEGORY = cat.ID_CATEGORY
                 where p.ID_USER = (%s)
                 """
         result = db.execute_query(query, (id_user,), fetch=True, dictionary=True)
