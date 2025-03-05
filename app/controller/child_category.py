16e7ccdd421b32a253e36ca841a31121a6db9d8e
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-05 09:42:02 +0100

    x

--- app/controller/child_category.py
+++ app/controller/child_category.py
@@ -5,19 +5,25 @@ import app.controller.parent_category as p_cat
 def fetch_all(id_user, unused):
     try:
         query = """
-            select ID_PARENT_CATEGORY as id, PARENT_CATEGORY_NAME as name, PARENT_CATEGORY_POSITION as position 
-            from PARENT_CATEGORY where ID_USER = (%s) and ID_PARENT_CATEGORY > 0
-            """
+                select par.ID_PARENT_CATEGORY as id, PARENT_CATEGORY_NAME as name, PARENT_CATEGORY_POSITION as position, 
+                    case when count(chi.ID_CATEGORY) > 0 then 0 else 1 end as can_be_deleted
+                from PARENT_CATEGORY par
+                left join CATEGORY chi
+                    on chi.ID_PARENT_CATEGORY = par.ID_PARENT_CATEGORY and chi.ID_USER = par.ID_USER
+                where par.ID_USER = (%s) and par.ID_PARENT_CATEGORY > 0
+                group by par.ID_PARENT_CATEGORY, PARENT_CATEGORY_NAME, PARENT_CATEGORY_POSITION
+
+                """
         parent_categories = db.execute_query(query, (id_user,), fetch=True, dictionary=True)
         for parent_category in parent_categories:
-            query_children = """
-                select cat.ID_CATEGORY as id, CATEGORY_NAME as name, ID_PARENT_CATEGORY as id_parent, 
-                    case when count(ID_TRANSACTION) > 0 then 0 else 1 end as can_be_deleted 
-                from CATEGORY cat left join TRANSACTION txn on txn.ID_USER = cat.ID_USER and txn.ID_CATEGORY = cat.ID_CATEGORY 
-                where cat.ID_USER = (%s) and ID_PARENT_CATEGORY = (%s) 
-                group by cat.ID_CATEGORY , CATEGORY_NAME , ID_PARENT_CATEGORY 
-                """
-            categories = db.execute_query(query_children, (id_user, parent_category['id']), fetch=True, dictionary=True)
+            query = """
+                    select cat.ID_CATEGORY as id, CATEGORY_NAME as name, ID_PARENT_CATEGORY as id_parent, 
+                        case when count(ID_TRANSACTION) > 0 then 0 else 1 end as can_be_deleted 
+                    from CATEGORY cat left join TRANSACTION txn on txn.ID_USER = cat.ID_USER and txn.ID_CATEGORY = cat.ID_CATEGORY 
+                    where cat.ID_USER = (%s) and ID_PARENT_CATEGORY = (%s) 
+                    group by cat.ID_CATEGORY , CATEGORY_NAME , ID_PARENT_CATEGORY 
+                    """
+            categories = db.execute_query(query, (id_user, parent_category['id']), fetch=True, dictionary=True)
             parent_category['child_categories'] = []
             for category in categories:
                 parent_category['child_categories'].append(category)
