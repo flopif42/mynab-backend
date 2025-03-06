a71548cf124bbe8d152afd4547fa8d61b0f8305c
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 11:26:06 +0100

    x

--- app/db.py
+++ app/db.py
@@ -11,20 +11,24 @@ class DbPool:
     @staticmethod
     def get_instance():
         if DbPool._pool_instance is None:
-            # Create the connection pool once at startup
-            DbPool._pool_instance = mysql.connector.pooling.MySQLConnectionPool(
-                pool_name="mypool",
-                pool_size=10,  # Max connections
-                pool_reset_session=True,
-                **DbPool._config
-            )
+            try:
+                # Create the connection pool once at startup
+                DbPool._pool_instance = mysql.connector.pooling.MySQLConnectionPool(
+                    pool_name="mypool",
+                    pool_size=10,  # Max connections
+                    pool_reset_session=True,
+                    **DbPool._config
+                )
+            except Exception as error:
+                print('db.get_instance() exception : %s %s' % (type(error).__name__, error))
+                raise
         return DbPool._pool_instance
 
     @staticmethod
     def get_connection():
         try:
             return DbPool.get_instance().get_connection()
-        except  Exception as error:
+        except Exception as error:
             print('db.get_connection() exception : %s %s' % (type(error).__name__, error))
             raise
 
