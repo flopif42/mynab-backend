27382f0ab8d58692768a45b0f1b193bfb3be6a49
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 11:37:13 +0100

    x

--- app/routes/about_routes.py
+++ app/routes/about_routes.py
@@ -1,3 +1,4 @@
+from mysql.connector.errors import DatabaseError
 import sys
 import json
 from http import HTTPStatus
@@ -9,19 +10,25 @@ about_bp = Blueprint('about', __name__)
 @about_bp.route('/hello', methods=['GET'])
 def hello():
     try:
+        api_version = json.load(open('version.json'))['version']
+    except:
+        print(f"Could not get API version.")
+        api_version = 'N/A'
+
+    try:
         query = "select VERSION from VERSION"
         result = db.execute_query(query, fetch=True)
         db_version = result[0][0]
         db_status = "Running"
-        api_version = json.load(open('version.json'))['version']
-        body = {
-            "API server status": "Running",
-            "API version": api_version,
-            "Database server status": db_status,
-            "Database version": db_version
-        }
-        return body, HTTPStatus.OK
+    except DatabaseError:
+        print(f"Could not connect to the database.")
+        db_status = 'Down'
+        db_version = 'N/A'
 
-    except ZeroDivisionError:
-        print(f"Exception: {err}")
-        return "", HTTPStatus.INTERNAL_SERVER_ERROR
+    body = {
+        "API server status": "Running",
+        "API version": api_version,
+        "Database server status": db_status,
+        "Database version": db_version
+    }
+    return body, HTTPStatus.OK
