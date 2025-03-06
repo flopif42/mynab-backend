1b4554bd109c47bc59ce7719ab0b9bb5635ab29a
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 15:45:58 +0100

    y

--- mynab_backend.py
+++ mynab_backend.py
@@ -1,24 +1,16 @@
 import json
-import requests
 import sys
-from flask import Flask
-from flask_cors import CORS, cross_origin
-from http import HTTPStatus
+import flask_cors
 import app
 from app.jwt_manager import JwtManager
 from app.sql_manager import SqlManager
 
-def load_configuration():
-    global _server_config
-
-    try:
-        _server_config = json.load(open('config.json'))
-        JwtManager.set_config(_server_config['JwtParams'])
-        SqlManager.set_config(_server_config['DatabaseParams'])
-    except Exception as err:
-        print(f"Could not load server configuration file. Exception: {err}")
-        sys.exit(1)
-
 mynab_app = app.create_app()
-load_configuration()
-CORS(mynab_app, **_server_config['CorsParams'])
+try:
+    server_config = json.load(open('config.json'))
+    JwtManager.set_config(server_config['JwtParams'])
+    SqlManager.set_config(server_config['DatabaseParams'])
+    flask_cors.CORS(mynab_app, **server_config['CorsParams'])
+except Exception as err:
+    print(f"Could not load server configuration file. Exception: {err}")
+    sys.exit(1)
