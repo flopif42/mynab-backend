6fbe801a9e934a80fd6e258f81e89f8c8465fc9c
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 12:10:04 +0100

    x

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,14 @@ from flask import Flask
 from app.jwt import JwtManager
 from app.routes.about_routes import about_bp
 from app.routes.transaction_routes import transaction_bp
+from app.routes.transfer_routes import transfer_bp
+from app.routes.payee_routes import payee_bp
+from app.routes.category_routes import category_bp
+from app.routes.account_routes import account_bp
+from app.routes.user_routes import user_bp
+from app.routes.budget_routes import budget_bp
 
 def create_app():
-    
-    from app.routes.transfer_routes import transfer_bp
-    from app.routes.payee_routes import payee_bp
-    from app.routes.category_routes import category_bp
-    from app.routes.account_routes import account_bp
-    from app.routes.user_routes import user_bp
-    from app.routes.budget_routes import budget_bp
-
     app = Flask(__name__)
     app.register_blueprint(transaction_bp)
     app.register_blueprint(transfer_bp)
