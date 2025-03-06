f9114d0be5cade849c47f49a1ba73d92d6fead1d
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 11:50:28 +0100

    x

--- app/routes/about_routes.py
+++ app/routes/about_routes.py
@@ -7,7 +7,7 @@ import app.db as db
 
 about_bp = Blueprint('about', __name__)
 
-@about_bp.route('/hello', methods=['GET'])
+@about_bp.route('/about', methods=['GET'])
 def hello():
     db_version = 'N/A'
     db_status = "Running"
