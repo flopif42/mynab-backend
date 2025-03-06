13d0600f088d9002bd2e818bcf3b5bdc1ae0b551
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 10:52:07 +0100

    z

--- app/routes/about_routes.py
+++ app/routes/about_routes.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from http import HTTPStatus
 from flask import Blueprint
@@ -7,7 +8,10 @@ about_bp = Blueprint('about', __name__)
 # -----------------------------------------------------------------------------
 @about_bp.route('/hello', methods=['GET'])
 def hello():
-    version_fd = open('version.json')
-    version = json.load(version_fd)['version']
-    body = { "Server status": "Running", "Version": version }
-    return body, HTTPStatus.OK
+    try:
+        version = json.load(open('version.json'))['version']
+        body = { "Server status": "Running", "Version": version }
+        return body, HTTPStatus.OK
+    except Exception as err:
+        print(f"Could not load version file. Exception: {err}")
+        return "", HTTPStatus.INTERNAL_SERVER_ERROR
