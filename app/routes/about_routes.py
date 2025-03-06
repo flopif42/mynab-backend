7ee9a23e0681b1164fdcae9a76c79a1574659893
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 11:12:37 +0100

    y

--- app/routes/about_routes.py
+++ app/routes/about_routes.py
@@ -2,14 +2,23 @@ import sys
 import json
 from http import HTTPStatus
 from flask import Blueprint
+import app.db as db
 
 about_bp = Blueprint('about', __name__)
 
 @about_bp.route('/hello', methods=['GET'])
 def hello():
     try:
-        version = json.load(open('version.json'))['version']
-        body = { "Server status": "Running", "Version": version }
+        query = "select VERSION from VERSION"
+        result = db.execute_query(query,, fetch=True)
+        db_version = result[0][0]
+        api_version = json.load(open('version.json'))['version']
+        body = {
+            "API server status": "Running",
+            "API version": api_version,
+            "Database server status": db_status,
+            "Database version": db_version
+        }
         return body, HTTPStatus.OK
     except Exception as err:
         print(f"Could not load version file. Exception: {err}")
