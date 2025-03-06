d2461fc91119a2fa2768ad227b0191a70b306a2a
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-06 10:34:38 +0100

    z

--- api_server.py
+++ api_server.py
@@ -4,23 +4,24 @@ import sys
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from http import HTTPStatus
-from app import create_app
+import app
 from app.jwt import JwtManager
 from app.db import DbPool
 
 def load_configuration():
+    global _server_config
+
     try:
-        server_config = json.load(open('config.json'))
+        _server_config = json.load(open('config.json'))
         JwtManager._config = server_config['JwtParams']
         DbPool._config = server_config['DatabaseParams']
-        return server_config
     except Exception as err:
         print(f"Could not load server configuration file. Exception: {err}")
         sys.exit(1)
 
-app = create_app()
-server_config = load_configuration()
-CORS(app, **server_config['CorsParams'])
+app = app.create_app()
+load_configuration()
+CORS(app, **_server_config['CorsParams'])
 
 # -----------------------------------------------------------------------------
 @app.route('/hello', methods=['GET'])
