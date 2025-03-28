b0a25486ee19bdbe14d4872f5d17de070c23129d
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-28 17:19:46 +0100

    y

--- app/controller/account.py
+++ app/controller/account.py
@@ -44,7 +44,7 @@ def delete(id_user, request_params):
         raise ValueError(400, "ID account can't be empty.")
     try:
         id_account = str(id_account).strip()
-        if id_account is '':
+        if id_account == '':
             raise ValueError(400, "ID account can't be empty.")
         id_account = int(id_account)
         if not is_valid(id_account):
