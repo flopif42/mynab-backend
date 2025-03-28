d3bb34dfc3d9223193b09f869c874f8300077fe1
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-28 15:26:37 +0100

    y

--- app/controller/account.py
+++ app/controller/account.py
@@ -19,7 +19,7 @@ def fetch_all(id_user, unused):
 
 def create(id_user, request_params):
     try:
-        if request_params['account_name'] in (None, ''):
+        if request_params['account_name'].strip() in (None, ''):
             raise ValueError("Account name can't be empty.")
         if request_params['account_type'] is None or int(request_params['account_type']) not in (1, 2):
             raise ValueError("Account type must be 1 or 2.")
