b0f253f1e91f68c6bfcffb946e53e91a4fee8314
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-28 15:08:19 +0100

    x

--- app/routes/account_routes.py
+++ app/routes/account_routes.py
@@ -1,50 +1,13 @@
 from flask import Blueprint
+from flasgger import swag_from
 from app.routes.handler import handle_route_action
 from app.controller import account
 
 account_bp = Blueprint('account', __name__)
 
-@account_bp.route('/account/list', methods=['GET'])
-def account_list():
-    return handle_route_action(account.fetch_all)
-
 @account_bp.route('/account/create', methods=['POST'])
+@swag_from('../docs/account/account_create.yml')
 def account_create():
-    """
-    Adds a new account.
-    ---
-    tags:
-      - Account
-    consumes:
-      - application/json
-    parameters:
-      - in: body
-        name: body
-        required: true
-        schema:
-          type: object
-          properties:
-            account_name:
-              type: string
-              description: The account name
-              example: Checking
-            account_type:
-              type: number
-              description: "The type of account (1: on-budget, 2: off-budget)"
-              example: 1
-          required:
-            - account_name
-            - account_type
-    responses:
-      201:
-        description: Account was created successfully
-      400:
-        description: Missing required parameters or incorrect parameters
-      401:
-        description: No user is logged in
-      500:
-        description: Internal server error
-    """
     try:
         return handle_route_action(account.create, create=True)
     except ValueError:
@@ -52,6 +15,10 @@ def account_create():
     except Exception as error:
         print(f"Exception in account_routes.account_create() : {type(error)} - {type(error).__name__} - {error}")
         return "", HTTPStatus.INTERNAL_SERVER_ERROR
+
+@account_bp.route('/account/list', methods=['GET'])
+def account_list():
+    return handle_route_action(account.fetch_all)
 
 @account_bp.route('/account/delete', methods=['POST'])
 def account_delete():
