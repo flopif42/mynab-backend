fdc4855f3a20def4a66e3fd4e4732129738f17e1
Florent Nguyen <nguyen.florent@gmail.com>
2025-03-28 15:02:12 +0100

    y

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -8,7 +8,7 @@ from app.jwt_manager import JwtManager
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/user/login', methods=['POST'])
-@swag_from('../docs/user_login.yml')
+@swag_from('../docs/user/user_login.yml')
 def user_login():
     try:
         id_user = user.login(request.json)
@@ -20,57 +20,13 @@ def user_login():
         return "", HTTPStatus.INTERNAL_SERVER_ERROR
 
 @user_bp.route('/user/sign-up', methods=['POST'])
+@swag_from('../docs/user/signup.yml')
 def sign_up():
-    """
-    Registers a new user.
-    ---
-    tags:
-      - User
-    consumes:
-      - application/json
-    parameters:
-      - in: body
-        name: body
-        required: true
-        schema:
-          type: object
-          properties:
-            first_name:
-              type: string
-              description: First name
-              example: Scrooge
-            last_name:
-              type: string
-              description: Last name
-              example: McDuck
-            email_address:
-              type: string
-              description: Email address
-              example: scrooge@mail.com
-            passphrase_md5:
-              type: string
-              description: MD5 encoded password
-              example: abcdef1234567890abcdef1234567890
-          required:
-            - email_address
-            - passphrase_md5
-
-    responses:
-      201:
-        description: User was created successfully
-      400:
-        description: Missing required parameters or incorrect parameters
-      409:
-        description: Could not create the user because the email address is already used
-      500:
-        description: Internal server error
-    """
     try:
         if not request.is_json or 'email_address' not in request.json or 'passphrase_md5' not in request.json:
             raise ValueError
         user.signup(request.json)
         return "", HTTPStatus.CREATED
-
     except ValueError:
         return "", HTTPStatus.BAD_REQUEST
     except RuntimeError as integrity_error:
@@ -81,30 +37,8 @@ def sign_up():
         return "", HTTPStatus.INTERNAL_SERVER_ERROR
 
 @user_bp.route('/user/check_email_available', methods=['GET'])
+@swag_from('../docs/user/check_email_available.yml')
 def check_email_available():
-    """
-    Checks if an email address is available for user creation.
-    ---
-    tags:
-      - User
-    parameters:
-      - name: email_address
-        in: query
-        required: true
-        type: string
-        description: Email address to check
-
-    responses:
-      200:
-        description: Email availability status
-        examples:
-          application/json:
-            available: true
-      400:
-        description: Missing or invalid email parameter
-      500:
-        description: Internal server error
-    """
     try:
         email_address = request.args.get("email_address")
         if not email_address:
@@ -117,26 +51,8 @@ def check_email_available():
         return "", HTTPStatus.INTERNAL_SERVER_ERROR
 
 @user_bp.route('/user/profile', methods=['GET'])
+@swag_from('../docs/user/user_profile.yml')
 def user_profile():
-    """
-    Gets information about a the logged in user.
-    ---
-    tags:
-      - User
-
-    responses:
-      200:
-        description: Information about the user
-        examples:
-          application/json:
-            first_name: Scrooge
-            last_name: McDuck
-            email_address: scrooge@mail.com
-      401:
-        description: No user is logged in
-      500:
-        description: Internal server error
-    """
     try:
         return handle_route_action(user.get_profile)
     except Exception as error:
