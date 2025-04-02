e40d71768a6730600386543b3cc15a110e4330d1
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-02 16:48:27 +0200

    z

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ from app.exceptions import OperationError
 
 def validate_not_empty(request, parameter_name):
     error_message = f"Parameter {parameter_name} can't be empty."
-	if not request.is_json or parameter_name not in request.json:
+    if not request.is_json or parameter_name not in request.json:
         raise OperationError(HTTPStatus.BAD_REQUEST, error_message)
     parameter = request_params[parameter_name]
     if parameter is None:
@@ -12,4 +12,4 @@ def validate_not_empty(request, parameter_name):
     parameter = parameter.strip()
     if parameter == '':
         raise OperationError(HTTPStatus.BAD_REQUEST, error_message)
-    return Parameter
+    return parameter
