50b1b05d37fb8e234160429eea873fc60ae190aa
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-04 11:44:24 +0200

    x

--- app/controller/transfer.py
+++ app/controller/transfer.py
@@ -45,7 +45,7 @@ def create(id_user, request):
                 '''
         return db.execute_query(query, (id_user, id_txn_outflow, id_txn_inflow), commit=True)
     except ValueError:
-        raise OperationError(HTTPStatus.BAD_REQUEST, "Invalid ID account.")
+        raise OperationError(HTTPStatus.BAD_REQUEST, "Invalid parameters.")
 
 def delete(id_user, id_transfer):
     try:
