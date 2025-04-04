08897f73d41b3f23a4be273f4c28e2c89ec69802
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-04 09:52:40 +0200

    y

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,7 +2,7 @@ class OperationError(Exception):
 	def __init__(self, http_status_code, error_message):
 		self.http_status_code = http_status_code
 		self.error_message = error_message
-		print('init called with {http_status_code} and {error_message}')
+		print(f'init called with {http_status_code} and {error_message}')
 
 	def get_status(self):
 		return self.http_status_code
