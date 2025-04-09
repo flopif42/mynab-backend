1ef974e4841ba995d4ad7ac392c2d9c278748a5d
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-09 17:38:20 +0200

    x

--- app/routes/handler.py
+++ app/routes/handler.py
@@ -15,14 +15,12 @@ response_status = {
 }
 
 def handle_route_action(action, mode='fetch', auth_required=True):
-    response_body = ''
     if auth_required and not JwtManager.check_token_valid(request):
-        return response_body, HTTPStatus.UNAUTHORIZED
+        return '', HTTPStatus.UNAUTHORIZED
     try:
         id_user = JwtManager.get_id_user_from_token(request) if auth_required else None
         result = action(id_user, request)
-        if result:
-            response_body = jsonify(result)
+        response_body = jsonify(result) if result else ''
         return response_body, response_status[mode]
     except ex.AccountOperationError as error:
         return { "error": error.error_message }, response_status[type(error).__name__]
@@ -30,4 +28,4 @@ def handle_route_action(action, mode='fetch', auth_required=True):
         return { "error": error.message }, error.status
     except Exception as error:
         print(f"Exception in handle_route_action() : {type(error).__name__} - {error}")
-        return response_body, HTTPStatus.INTERNAL_SERVER_ERROR
+        return '', HTTPStatus.INTERNAL_SERVER_ERROR
