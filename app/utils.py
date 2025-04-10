6e433a26c70e313f046455dfefd893a59765751c
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-10 14:07:23 +0200

    x

--- app/utils.py
+++ app/utils.py
@@ -9,22 +9,18 @@ def validate_not_empty(request, parameter_name):
     Return values : If the parameter is not empty, returns a stripped version of the parameter
     Exception : Raises OperationError with the 400 HTTP code if the parameter is empty
     """
-    try:
-        error_message = f"Parameter {parameter_name} can't be empty."
-        if request.method == 'GET' or request.method == 'DELETE':
-            if not parameter_name in request.args:
-                raise MyOperationError(error_message)
-            parameter = request.args.get(parameter_name)
-        else:
-            if not request.is_json or parameter_name not in request.json:
-                raise MyOperationError(error_message)
-            parameter = request.json[parameter_name]
-        if parameter is None:
+    error_message = f"Parameter {parameter_name} can't be empty."
+    if request.method == 'GET' or request.method == 'DELETE':
+        if not parameter_name in request.args:
             raise MyOperationError(error_message)
-        parameter = str(parameter).strip()
-        if parameter == '':
+        parameter = request.args.get(parameter_name)
+    else:
+        if not request.is_json or parameter_name not in request.json:
             raise MyOperationError(error_message)
-        return parameter
-    except Exception as error:
-        print(f"Exception in validate_not_empty() : {type(error).__name__} - {error} parameter_name = {parameter_name}")
-        raise error
+        parameter = request.json[parameter_name]
+    if parameter is None:
+        raise MyOperationError(error_message)
+    parameter = str(parameter).strip()
+    if parameter == '':
+        raise MyOperationError(error_message)
+    return parameter
