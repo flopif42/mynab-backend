447d3567ee4a665b748462e378ce447982bb05cd
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-10 11:48:02 +0200

    y

--- app/routes/handler.py
+++ app/routes/handler.py
@@ -6,13 +6,20 @@ import app.exceptions as ex
 response_status = {
     'fetch': HTTPStatus.OK,
     'create': HTTPStatus.CREATED,
-    'delete': HTTPStatus.NO_CONTENT,
-    'update': HTTPStatus.NO_CONTENT,
-    'AccountInvalidParametersError': HTTPStatus.BAD_REQUEST,
-    'AccountNotFoundError': HTTPStatus.NOT_FOUND,
+    ('delete', 'update'): HTTPStatus.NO_CONTENT,
+    'InvalidParametersError': HTTPStatus.BAD_REQUEST,
+    ('AccountNotFoundError', 'PayeeNotFoundError'): HTTPStatus.NOT_FOUND,
     'AccountPermissionError': HTTPStatus.FORBIDDEN,
     'AccountNotEmptyError': HTTPStatus.CONFLICT
 }
+
+def get_status_code(mode):
+    if response_status.get(mode):
+        return response_status.get(mode)
+    for key in response_status.keys():
+        if isinstance(key, tuple) and mode in key:
+            return response_status.get(key)
+    return HTTPStatus.INTERNAL_SERVER_ERROR
 
 def handle_route_action(action, mode='fetch', auth_required=True):
     if auth_required and not JwtManager.check_token_valid(request):
@@ -21,9 +28,9 @@ def handle_route_action(action, mode='fetch', auth_required=True):
         id_user = JwtManager.get_id_user_from_token(request) if auth_required else None
         result = action(id_user, request)
         response_body = jsonify(result) if result else ''
-        return response_body, response_status[mode]
+        return response_body, get_status_code[mode]
     except ex.MyOperationError as error:
-        return { "error": error.error_message }, response_status[type(error).__name__]
+        return { "error": error.error_message }, get_status_code[type(error).__name__]
     except ex.OperationError as error:
         return { "error": error.message }, error.status
     except Exception as error:
