4cc763745321576f47766544746bfcee716e543c
Florent Nguyen <nguyen.florent@gmail.com>
2025-04-10 11:56:27 +0200

    y

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 from flask import request
 from http import HTTPStatus
-from app.exceptions import OperationError
+from app.exceptions import MyOperationError
 
 def validate_not_empty(request, parameter_name):
     """
@@ -13,17 +13,17 @@ def validate_not_empty(request, parameter_name):
         error_message = f"Parameter {parameter_name} can't be empty."
         if request.method == 'GET' or request.method == 'DELETE':
             if not parameter_name in request.args:
-                raise OperationError(HTTPStatus.BAD_REQUEST, error_message)
+                raise MyOperationError(error_message)
             parameter = request.args.get(parameter_name)
         else:
             if not request.is_json or parameter_name not in request.json:
-                raise OperationError(HTTPStatus.BAD_REQUEST, error_message)
+                raise MyOperationError(error_message)
             parameter = request.json[parameter_name]
         if parameter is None:
-            raise OperationError(HTTPStatus.BAD_REQUEST, error_message)
+            raise MyOperationError(error_message)
         parameter = str(parameter).strip()
         if parameter == '':
-            raise OperationError(HTTPStatus.BAD_REQUEST, error_message)
+            raise MyOperationError(error_message)
         return parameter
     except Exception as error:
         print(f"Exception in validate_not_empty() : {type(error).__name__} - {error} parameter_name = {parameter_name}")
